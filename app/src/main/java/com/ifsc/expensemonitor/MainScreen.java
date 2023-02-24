a3d23735d3d700c3eac9462f28709352fd8bc849
Sabasi <samuelbds1@outlook.com>
2023-02-24 14:43:09 -0300

    firebase implementation test

--- app/src/main/java/com/ifsc/expensemonitor/MainScreen.java
+++ app/src/main/java/com/ifsc/expensemonitor/MainScreen.java
@@ -2,10 +2,10 @@ package com.ifsc.expensemonitor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -13,6 +13,14 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.expense_list);
+        databaseChange();
+    }
+
+    void databaseChange(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("caos");
+
+        myRef.setValue("Hello, World!");
     }
 
 }
