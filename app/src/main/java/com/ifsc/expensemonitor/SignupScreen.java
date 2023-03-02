b2a27e5b995daf32fcd0d3a4bdaa0d7f6ba75aad
Sabasi <samuelbds1@outlook.com>
2023-03-02 16:28:36 -0300

    comment fix

--- app/src/main/java/com/ifsc/expensemonitor/SignupScreen.java
+++ app/src/main/java/com/ifsc/expensemonitor/SignupScreen.java
@@ -11,4 +11,36 @@ public class SignupScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_screen);
     }
+
+//    protected void databaseTest(){
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("Teste");
+//        TextView valor = ((TextView)findViewById(R.id.valor));
+//        TextView input = ((TextView)findViewById(R.id.TestText));
+//        Button enviar = ((Button)findViewById(R.id.enviar));
+//
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                String value = dataSnapshot.getValue(String.class);
+//                valor.setText(value);
+//                Log.d(TAG, "Value is: " + value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w(TAG, "Failed to read value.", error.toException());
+//            }
+//        });
+//
+//        enviar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                myRef.setValue(input.getEditableText().toString());
+//                input.setText("");
+//            }
+//        });
+//   }
+
 }
