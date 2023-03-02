54bb0e8c542e0e1a1e1f936674ee7b0919cffe95
Sabasi <samuelbds1@outlook.com>
2023-03-02 16:26:34 -0300

    Testdatabase

--- app/src/main/java/com/ifsc/expensemonitor/WelcomeScreen.java
+++ app/src/main/java/com/ifsc/expensemonitor/WelcomeScreen.java
@@ -1,11 +1,21 @@
 package com.ifsc.expensemonitor;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class WelcomeScreen extends AppCompatActivity {
 
@@ -23,7 +33,7 @@ public class WelcomeScreen extends AppCompatActivity {
         nextPageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(WelcomeScreen.this, ExpensesList.class));
+                startActivity(new Intent(WelcomeScreen.this, TestActivity.class));
             }
         });
     }
@@ -37,4 +47,35 @@ public class WelcomeScreen extends AppCompatActivity {
             }
         });
     }
+
+
+//    protected void databaseTest(){
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("Teste");
+//        TextView valor = ((TextView)findViewById(R.id.valor));
+//        TextView input = ((TextView)findViewById(R.id.TestText));
+//        Button enviar = ((Button)findViewById(R.id.enviar));
+//
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                String value = dataSnapshot.getValue(String.class);
+//                valor.setText(value);
+//                Log.d(TAG, "Value is: " + value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w(TAG, "Failed to read value.", error.toException());
+//            }
+//        });
+//
+//        enviar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                myRef.setValue(input.getEditableText().toString());
+//                input.setText("");
+//            }
+//        });//   }
 }
