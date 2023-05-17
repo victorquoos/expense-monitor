20837c5b24147f1b8e9b421e6f8d62b5822df69e
Victor Quoos <117126925+victorquoos@users.noreply.github.com>
2023-05-17 18:08:18 -0300

    navegação: usuário já logado

--- app/src/main/java/com/ifsc/expensemonitor/MainActivity.java
+++ app/src/main/java/com/ifsc/expensemonitor/MainActivity.java
@@ -1,14 +1,36 @@
 package com.ifsc.expensemonitor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.ifsc.expensemonitor.database.FirebaseSettings;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        verifyIfUserIsLoggedIn();
+    }
+
+    private void verifyIfUserIsLoggedIn() {
+        auth = FirebaseSettings.getFirebaseAuth();
+
+        //auth.signOut();
+
+        if (auth.getCurrentUser() == null) {
+            Navigation.findNavController(this, R.id.fragmentContainerView).navigate(R.id.welcomeFragment);
+        }
     }
 }
