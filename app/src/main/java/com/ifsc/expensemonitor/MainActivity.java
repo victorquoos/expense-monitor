4eaf1188c4a5aae0ed2e3152e716ed20b55686b6
CeresQuaso <samuelbds1@outlook.com>
2023-06-11 20:50:29 -0300

    notificação.

--- app/src/main/java/com/ifsc/expensemonitor/MainActivity.java
+++ app/src/main/java/com/ifsc/expensemonitor/MainActivity.java
@@ -1,26 +1,49 @@
 package com.ifsc.expensemonitor;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.view.ContextThemeWrapper;
-import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 
 import android.content.Context;
-import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.TypedValue;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.ifsc.expensemonitor.database.FirebaseSettings;
+import com.ifsc.expensemonitor.notifications.NotificationWorker;
+
+import java.security.cert.CertPathBuilder;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static NotificationManager notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
+
+        PeriodicWorkRequest notificacaoWork = new PeriodicWorkRequest.Builder(NotificationWorker.class, 15, TimeUnit.MINUTES).build();
+        WorkManager.getInstance(this).enqueue(notificacaoWork);
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "";
+            String description = "";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("notifications_channel", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
---
+++ app/src/main/java/com/ifsc/expensemonitor/notifications/NotificationWorker.java
@@ -0,0 +1,92 @@
+package com.ifsc.expensemonitor.notifications;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.ifsc.expensemonitor.MainActivity;
+import com.ifsc.expensemonitor.R;
+import com.ifsc.expensemonitor.database.FirebaseSettings;
+import com.ifsc.expensemonitor.database.Occurrence;
+import com.ifsc.expensemonitor.database.SimpleDate;
+
+public class NotificationWorker extends Worker {
+
+    private Context context;
+    private int overdueOccurrences = 0;
+    private int pendingOccurrences = 0;
+
+    public NotificationWorker(
+            @NonNull Context context,
+            @NonNull WorkerParameters params) {
+        super(context, params);
+        this.context = context;
+    }
+
+    @Override
+    public Result doWork() {
+
+        DatabaseReference occurrencesRef = FirebaseSettings.getOccurrencesReference();
+
+        occurrencesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot yearSnapshot : snapshot.getChildren()) {
+                    for (DataSnapshot monthSnapshot : yearSnapshot.getChildren()) {
+                        for (DataSnapshot occurrenceSnapshot : monthSnapshot.getChildren()) {
+                            Occurrence occurrence = occurrenceSnapshot.getValue(Occurrence.class);
+                            if (occurrence != null) {
+                                if (!occurrence.isPaid()) {
+                                    if (occurrence.getDate().isBefore(SimpleDate.today())) {
+                                        overdueOccurrences++;
+                                    } else if (occurrence.getDate().isBefore(SimpleDate.today().plusDays(7))) {
+                                        pendingOccurrences++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (overdueOccurrences > 0) {
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notifications_channel")
+                            .setSmallIcon(R.drawable.ic_circle)
+                            .setContentTitle("Expense Monitor")
+                            .setContentText("Você tem " + overdueOccurrences + " despesas atrasadas!")
+                            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                            .setAutoCancel(true);
+
+                    MainActivity.notificationManager.notify(1, builder.build());
+                }
+
+                if (pendingOccurrences > 0) {
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notifications_channel")
+                            .setSmallIcon(R.drawable.ic_circle)
+                            .setContentTitle("Expense Monitor")
+                            .setContentText("Você tem " + pendingOccurrences + " despesas próximas do vencimento!")
+                            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                            .setAutoCancel(true);
+
+                    MainActivity.notificationManager.notify(2, builder.build());
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return Result.success();
+    }
+}
+
